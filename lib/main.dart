9d2a148014546d31ebb5c3a69c8867a6a51487de
Umer Tahir <UmerTahir6@gmail.com>
2020-07-16 15:21:49 +0500

    My UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:quizzler/quiz_brain.dart';
+import 'package:flutter/services.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(Quizzler());
+
+Quizbrain quizBrain = Quizbrain();
 
 class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
+        appBar: AppBar(
+          elevation: 1,
+          backgroundColor: Colors.redAccent,
+          title: Text('Quizzler'),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
-          ),
+          child: QuizPage(),
         ),
       ),
     );
@@ -25,6 +39,49 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> _scoreKeeper = [];
+
+  //bool _endOfQuiz = null;
+
+  _resetAtEnd(context) {
+
+    Alert(
+      style: AlertStyle(
+          isOverlayTapDismiss: false, animationType: AnimationType.fromTop),
+      context: context,
+      title: 'Quiz Completed!',
+      desc: 'Quiz will be restarted',
+      type: AlertType.success,
+      buttons: [
+        DialogButton(
+          child: Text('OK'),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ],
+    ).show();
+    _scoreKeeper = [];
+    quizBrain.resetQuiz();
+  }
+
+  void checkClose(bool value) {
+    final player = AudioCache();
+    if (quizBrain.getQuestionAnswer() == value) {
+      player.play('1.wav');
+      _scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+    } else {
+      player.play('2.wav');
+      _scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+    }
+    setState(() {
+      //go to next question and if there is no question nextQuestion will return false
+      if (quizBrain.nextQuestion() == false) {
+        // if at end then show end and reset alert
+        _resetAtEnd(context);
+        player.play('finish.wav');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -32,25 +89,64 @@ class _QuizPageState extends State<QuizPage> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
-          flex: 5,
+          flex: 8,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.black54,
+              ),
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 5,
+                    child: Center(
+                      child: Text(
+                        quizBrain.getQuestionText(),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Center(
+                      child: FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Text(
+                          '?',
+                          style: TextStyle(
+                              color: Colors.white,
+                              shadows: [
+                                Shadow(
+                                    color: Colors.green,
+                                    blurRadius: 3,
+                                    offset: Offset.fromDirection(2))
+                              ],
+                              fontSize: 70,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
         ),
         Expanded(
+          flex: 2,
           child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
+            padding: EdgeInsets.fromLTRB(15, 0, 15, 10),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
               textColor: Colors.white,
               color: Colors.green,
               child: Text(
@@ -60,17 +156,18 @@ class _QuizPageState extends State<QuizPage> {
                   fontSize: 20.0,
                 ),
               ),
-              onPressed: () {
-                //The user picked true.
-              },
+              onPressed: () => checkClose(true),
             ),
           ),
         ),
         Expanded(
+          flex: 2,
           child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            padding: EdgeInsets.fromLTRB(15, 0, 15, 10),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              color: Colors.deepOrange,
               child: Text(
                 'False',
                 style: TextStyle(
@@ -78,13 +175,24 @@ class _QuizPageState extends State<QuizPage> {
                   color: Colors.white,
                 ),
               ),
-              onPressed: () {
-                //The user picked false.
-              },
+              onPressed: () => checkClose(false),
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Expanded(
+          flex: 1,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 0, 15, 10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.black45,
+                  borderRadius: BorderRadius.circular(6)),
+              child: Row(
+                children: _scoreKeeper,
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,34 +1,10 @@
-# My Done App
+import 'package:quizzler/question.dart';
 
-<img alt="UI flow" src="https://github.com/Bytepie/quizzler-flutter/1s.png">
+class Quizbrain {
+  int _questionNumber = 0;
 
-# Quizzler ❓
-
-## Our Goal
-
-In this tutorial we will be reviewing Stateful and Stateless Widgets as well as learning about the fundamental building blocks of Object Oriented Programming (OOP) - Classes and Objects. 
-
-
-## What you will create
-
-We’re going to make a quiz app that tests your general knowledge. As one of the most popular types of apps on the app stores, you can create your own quiz for other people to enjoy!
-
-![Finished App](https://github.com/londonappbrewery/Images/blob/master/quizzler-demo.gif)
-
-## What you will learn
-
-- Modularising your code into separate classes.
-- Dart classes and objects.
-- Using class constructors.
-- Extracting Widgets to refactor your code.
-- private and public modifiers in Dart.
-- How to use Dart lists.
-- The difference between var, const and final.
-
-## Code Snippet for Project
-
-```
-Question('Some cats are actually allergic to humans', true),
+  final List<Question> _questionBank = [
+    Question('Some cats are actually allergic to humans', true),
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
     Question('A slug\'s blood is green.', true),
@@ -53,10 +29,29 @@ Question('Some cats are actually allergic to humans', true),
     Question(
         'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
         true),
+    Question('Ocean is blue.', true)
+  ];
 
-```
+  bool nextQuestion(){
+    if (_questionNumber < _questionBank.length-1){
+      print(_questionNumber.toString()+' < '+_questionBank.length.toString());
+      _questionNumber++;
+      return true;
+    }else {
+      return false;
+    }
+  }
 
+  String getQuestionText(){
+    return _questionBank[_questionNumber].questionText;
+  }
 
->This is a companion project to The App Brewery's Complete Flutter Development Bootcamp, check out the full course at [www.appbrewery.co](https://www.appbrewery.co/)
+  bool getQuestionAnswer(){
+    return _questionBank[_questionNumber].questionAnswer;
+  }
 
-![End Banner](https://github.com/londonappbrewery/Images/blob/master/readme-end-banner.png)
+  void resetQuiz(){
+    _questionNumber = 0;
+  }
+
+}
